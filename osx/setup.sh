5f1d549d3f5c5ea0aaf0f094c59db9631474de46
Sven Kang <sven.c.kang@gmail.com>
2020-12-05 11:37:11 -0600

    OSX defaults update

--- osx/setup.sh
+++ osx/setup.sh
@@ -3,6 +3,10 @@
 echo 'Please manually setup osx settings...'
 mkdir ~/dev
 # finder settings
+echo 'Please manually setup finder settings...'
 # dock settings
+echo 'Please manually setup dock settings...'
 # menu settings
+echo 'Please manually setup menu settings...'
 # mouse settings
+defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
