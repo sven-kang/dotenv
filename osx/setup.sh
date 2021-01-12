9c2dcc602ed66747d66435190b37fd232c7d01d8
Sven Kang <sven.c.kang@gmail.com>
2021-01-12 17:57:16 -0600

    updated keyboard settings change and iterm settings change

--- osx/setup.sh
+++ osx/setup.sh
@@ -11,3 +11,5 @@ echo 'Please manually setup dock settings...'
 echo 'Please manually setup menu settings...'
 # mouse settings
 defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
+# keyboard settings
+echo 'Please manually setup the keyboard settings...'
